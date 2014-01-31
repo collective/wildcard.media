fabcb6b77a1b11f0fef3e84d2b3c9a72ffc75ff3
Nathan Van Gheem <vangheem@gmail.com>
2014-01-31 08:46:08 -0600

    fix video type definition

--- wildcard/media/upgrades.py
+++ wildcard/media/upgrades.py
@@ -8,3 +8,8 @@ def upgrade_css(context, logger=None):
     setup = getToolByName(context, 'portal_setup')
     setup.runImportStepFromProfile(PROFILE_ID, 'cssregistry')
 
+
+def upgrade_types(context, logger=None):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(PROFILE_ID, 'typeinfo')
+
