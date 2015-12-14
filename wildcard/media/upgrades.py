697fc72d3a420e57e41e9d310f7e45d3ad89a401
Daniel Marks <dmarks@pretaweb.com>
2015-12-14 16:00:25 +1030

    Add an upgrade step to apply old infile/outfile options to each format specific option

--- wildcard/media/upgrades.py
+++ wildcard/media/upgrades.py
@@ -1,5 +1,6 @@
 from Products.CMFPlone.utils import getToolByName
 from Products.CMFPlone.utils import getFSVersionTuple
+from wildcard.media.settings import GlobalSettings
 
 
 PROFILE_ID = 'profile-wildcard.media:default'
@@ -29,3 +30,20 @@ def upgrade_registry(context, logger=None):
 def upgrade_to_2(context):
     upgrade_registry(context)
     upgrade_resources(context)
+
+
+def upgrade_to_2003(context):
+    portal = getToolByName(context, 'portal_url').getPortalObject()
+    settings = GlobalSettings(portal)
+    # Apply old in/outfile options to each new format specific option
+    old_outfileopt = settings.convert_outfile_options
+    old_infileopt = settings.convert_infile_options
+
+    if old_outfileopt:
+        settings.avconv_out_mp4 = old_outfileopt
+        settings.avconv_out_ogg = old_outfileopt
+        settings.avconv_out_webm = old_outfileopt
+    if old_infileopt:
+        settings.avconv_in_mp4 = old_infileopt
+        settings.avconv_in_ogg = old_infileopt
+        settings.avconv_in_webm = old_infileopt
