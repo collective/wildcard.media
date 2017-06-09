962600a02b1a44c4264dc6850d838de089643afc
Giacomo Monari <arsenico13@hotmail.it>
2017-06-09 14:22:10 +0200

    [new][fix] changes the type for VideoEmbedCode class. This is now the view for the internal videos.

--- wildcard/media/browser/videoembedcode.py
+++ wildcard/media/browser/videoembedcode.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from zope.interface import Interface
+from wildcard.media.interfaces import IVideoEnabled
+from zope.interface import implements
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 
-class VideoEmbedCode(Interface):
 
-    pass
+class VideoEmbedCode(object):
+
+    implements(IVideoEnabled)
+    template = ViewPageTemplateFile('templates/internalvideo_template.pt')
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        return self.template()
