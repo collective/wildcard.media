8f1a02a2f21c4afdd06edeba7bb5bfa847efe7df
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2021-09-03 15:20:17 -0500

    Add names to async tasks.

--- wildcard/media/tasks.py
+++ wildcard/media/tasks.py
@@ -9,26 +9,27 @@ except ImportError:
 import time
 
 
-@task.as_admin(autoretry_for=(POSKeyError,), retry_backoff=5)
+@task.as_admin(autoretry_for=(POSKeyError,), retry_backoff=5,
+               name='wildcard.media.convertVideoFormats')
 def convertVideoFormats(context):
     convert.convertVideoFormats(context)
 
 
-@task.as_admin()
+@task.as_admin(name='wildcard.media.uploadToYouTube')
 def uploadToYouTube(context):
     youtube.uploadToYouTube(context)
 
 
-@task.as_admin()
+@task.as_admin(name='wildcard.media.removeFromYouTube')
 def removeFromYouTube(context):
     youtube.removeFromYouTube(context)
 
 
-@task.as_admin()
+@task.as_admin(name='wildcard.media.updateYouTubePermissions')
 def updateYouTubePermissions(context):
     youtube.updateYouTubePermissions(context)
 
 
-@task.as_admin()
+@task.as_admin(name='wildcard.media.editYouTubeVideo')
 def editYouTubeVideo(context):
     youtube.editYouTubeVideo(context)
